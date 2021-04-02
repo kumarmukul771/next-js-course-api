cc60bd5c98205198b641d18b8d5c624c6f67a568
kumarmukul771 <kumarmukul771@gmail.com>
2021-04-03 00:25:30 +0530

    Working with post api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const email = emailInputRef.current.value;
+    const feedback = feedbackInputRef.current.value;
+
+    const reqBody = { email, feedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
